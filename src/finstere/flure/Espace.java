f5a2fcab184d02c00cfd6090db9f459b498b6f1c
2goat2 <65475043+2goat2@users.noreply.github.com>
2022-03-04 21:30:06 +0100

    Espace - Joueur - PionMonstre
    Ajouter des paramètres dans la classe Espace, Joueur, et PionMonstre

--- src/finstere/flure/Espace.java
+++ src/finstere/flure/Espace.java
@@ -9,15 +9,182 @@ package finstere.flure;
  *
  * @author nadim
  */
-
- 
 public class Espace {
-    
-    private boolean occupe = true;
-    
-    public Espace(boolean s){
-        this.occupe = s;
+
+    //Permet de savoir si la casse est occupée par un autre pion ou non.
+    private boolean estOccupe = true;
+
+    //coordonnée de l'espace
+    private int x;
+    private int y;
+
+    //Permet de créer un objet et le mettre dans l'espace
+    private Object obj = null;
+
+    //Permet d'afficher l'espace
+    private String affichage = "| |";
+
+    //Orientations
+    private final int NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4;
+
+    //Constructeurs
+    /**
+     * permet de créer un espace vide standard
+     */
+    public Espace() {
+    }
+
+    /**
+     * permet de créer un espace vide standard mais le définit comme utilisable
+     * ou non
+     *
+     * @param estOccupe True si l'espace est occupée, sinon false
+     */
+    public Espace(boolean estOccupe) {
+        this.estOccupe = estOccupe;
+        if (!estOccupe) {
+            affichage = "|X|";
+        }
 
     }
 
+    /**
+     *
+     * permet de créer un espace vide standard par rapport à ses x et y
+     *
+     * @param x
+     * @param y
+     * @param estOccupe True si l'espace est occupée, sinon false
+     */
+    public Espace(int x, int y, boolean estOccupe) {
+
+        this.x = x;
+        this.y = y;
+        this.estOccupe = estOccupe;
+        if (!estOccupe) {
+            affichage = "|X|";
+        }
+
+    }
+
+    /**
+     *
+     * permet de créer un espace qui contient un objet par rapport à ses x et y
+     *
+     * @param x
+     * @param y
+     * @param obj l'objet lequel on vas le mettre dans cet espace
+     */
+    public Espace(int x, int y, Object obj) {
+
+        this.x = x;
+        this.y = y;
+        ajouterObjet(obj);
+
+    }
+
+    /**
+     * Ajouter l'objet dans l'espace
+     *
+     * @param obj The object to be added
+     * @return Retourne true si l'objet a été bien ajouté, sinon false
+     */
+    public boolean ajouterObjet(Object obj) {
+
+        if (this.obj != null) {
+            return false;
+        } else if (estOccupe) {
+            this.obj = obj;
+            return false;
+        }
+        this.obj = obj;
+        if (obj == null) {
+            affichage = "| |";
+        } else if (obj.getClass().equals(Joueur.class)) {
+            affichage = "|" + ((Joueur) obj).getNom().charAt(0) + "|";
+        } else if (obj.getClass().equals(Pierre.class)) {
+            affichage = "|🪨|";
+        } else if (obj.getClass().equals(Pivot90.class)) {
+            affichage = "|⮔|";
+        } else if (obj.getClass().equals(Pivot180.class)) {
+            affichage = "|⇼|";
+        } else if (obj.getClass().equals(PionMonstre.class)) {
+            switch (((PionMonstre) obj).getDirection()) {
+                case NORTH:
+                    affichage = "|⌃|";
+                    break;
+                case EAST:
+                    affichage = "|🢒|";
+                    break;
+                case SOUTH:
+                    affichage = "|⌄|";
+                    break;
+                case WEST:
+                    affichage = "|🢐|";
+                    break;
+                default:
+                    affichage = "|M|";
+            }
+        } else {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Getters AND Setters
+     */
+    // POUR X
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int a) {
+        this.x = a;
+    }
+
+    // POUR Y
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int a) {
+        this.y = a;
+    }
+
+    //POUR estOccupe
+    public boolean isOccupee() {
+        return estOccupe;
+    }
+
+    public void setOccupee(boolean occupe) {
+        this.estOccupe = occupe;
+    }
+
+    //POUR l'objet
+    public Object getObjet() {
+        return obj;
+    }
+
+    public void setObjet(Object a) {
+        this.obj = a;
+    }
+
+    // Méthode toString
+    public String toString() {
+        return affichage;
+    }
+
+    // Méthode pour supprimer l'objet d'un espace
+    public Object supprimerObjet() {
+        Object temp = obj;
+        obj = null;
+        if (estOccupe) {
+            affichage = "| |";
+        } else {
+            affichage = "|X|";
+        }
+        return temp;
+    }
 }
--- src/finstere/flure/PionMonstre.java
+++ src/finstere/flure/PionMonstre.java
@@ -10,5 +10,25 @@ package finstere.flure;
  * @author nadim
  */
 public class PionMonstre {
-    
+
+    private int direction;
+    private final int NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4;
+
+    public PionMonstre(int direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * SETTER
+     *
+     * @param direction permet de modifier la direction du monstre
+     */
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
 }
